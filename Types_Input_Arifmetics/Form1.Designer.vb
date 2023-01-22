6b65ea8ebcf0d8071f2482bfb0244df791abf4d5
Daria N <smb_udk@vk.com>
2023-01-22 13:38:50 +0300

    Add files via upload

---
+++ Types_Input_Arifmetics/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.CadetBlue
+        Me.Button1.Font = New System.Drawing.Font("Showcard Gothic", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.Color.Coral
+        Me.Button1.Location = New System.Drawing.Point(128, 330)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(307, 96)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "start"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Chartreuse
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.Button2.Font = New System.Drawing.Font("Vladimir Script", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.ForeColor = System.Drawing.Color.CornflowerBlue
+        Me.Button2.Location = New System.Drawing.Point(441, 372)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(347, 54)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Variables"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Chocolate
+        Me.Button3.Font = New System.Drawing.Font("Times New Roman", 25.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Button3.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button3.Location = New System.Drawing.Point(441, 231)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(347, 135)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Diffetent DataTypes"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button4.Font = New System.Drawing.Font("Tempus Sans ITC", 22.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.Location = New System.Drawing.Point(1, 366)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(121, 60)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Input"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(188, 33)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(379, 27)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(102, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 20)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "First Name"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(188, 97)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(379, 27)
+        Me.TextBox2.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(102, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 20)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Last Name"
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Button5.Font = New System.Drawing.Font("Yu Gothic Medium", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Button5.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button5.Location = New System.Drawing.Point(201, 149)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(362, 43)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "New Way for Input"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Items.AddRange(New Object() {"музыкант", "актёр", "учёный", "строитель", "программист"})
+        Me.ListBox1.Location = New System.Drawing.Point(1, 196)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(121, 164)
+        Me.ListBox1.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(1, 173)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 20)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Ocupation" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(128, 251)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(53, 27)
+        Me.TextBox3.TabIndex = 12
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(128, 284)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(54, 27)
+        Me.TextBox4.TabIndex = 13
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Button6.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button6.Location = New System.Drawing.Point(189, 296)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(82, 28)
+        Me.Button6.TabIndex = 14
+        Me.Button6.Text = "Arifmatic"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Segoe UI Historic", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button7.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button7.Location = New System.Drawing.Point(692, 12)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(96, 64)
+        Me.Button7.TabIndex = 15
+        Me.Button7.Text = "Complex Expressions"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.ForeColor = System.Drawing.Color.Crimson
+        Me.Name = "Form1"
+        Me.Text = "Firts Name"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+End Class
---
+++ Types_Input_Arifmetics/Form1.vb
@@ -0,0 +1,115 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("While the cat's away, the mice will play")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim strFirtsName As String
+        strFirtsName = "Sasha"
+
+        MessageBox.Show("Hello " & strFirtsName & "! " & "Have a good day!")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim strMake As String
+        Dim StrModule As String
+        Dim iDoors As Integer
+        Dim color As String
+        Dim bTaxed As Boolean
+        Dim iEngineSize As Integer
+        Dim decPrice As Decimal
+        Dim dtDataRegistered As Date
+
+        strMake = "Ford"
+        StrModule = "Escort"
+        iDoors = 5
+        color = "black"
+        bTaxed = True
+        iEngineSize = 1200
+        decPrice = 999.99
+        dtDataRegistered = #11/2/2023#
+
+        MessageBox.Show("This car is " & strMake & StrModule & ". It has " & iDoors & " doors. It is " & color &
+                        ". Is it taxed - " & bTaxed & ". Its engine is " & iEngineSize & ". It costs " & decPrice &
+                        ". It was registreded " & dtDataRegistered)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'Dim strFisrtName As String
+        'strFisrtName = InputBox("Please enter your fisrt name") 
+        'MessageBox.Show("Hello " & strFisrtName)
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button5_Click_1(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Dim srtFirstName As String
+        Dim strLastName As String
+        Dim strOcucupation As String
+
+        srtFirstName = TextBox1.Text
+        strLastName = TextBox2.Text
+
+        strOcucupation = ListBox1.SelectedItem
+
+        MessageBox.Show(srtFirstName & " " & strLastName & " is a " & strOcucupation)
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add("Повар")
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        Dim inum1 As Integer
+        Dim inum2 As Integer
+        Dim iResultSum As Integer
+        Dim iResultSub As Integer
+
+        inum1 = TextBox3.Text
+        inum2 = TextBox4.Text
+
+        iResultSum = inum1 + inum2
+        iResultSub = inum1 - inum2
+        MessageBox.Show(iResultSum)
+        MessageBox.Show(iResultSub)
+
+        ' ^ возвести в степень 
+        ' \ разделить на цело 
+        ' Mod показывает остаток 
+
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim decPrice As Decimal
+        Dim iQuantity As Integer
+        Dim decTotalCost As Decimal
+        Dim decDescount As Decimal
+
+        decPrice = 5
+        iQuantity = 10
+        decDescount = 2
+
+        'используем "()" для определения порядка действия
+
+        decTotalCost = (decPrice - decDescount) * iQuantity
+
+        MessageBox.Show(decTotalCost)
+    End Sub
+End Class
